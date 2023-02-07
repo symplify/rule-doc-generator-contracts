c3dbfbf51c2a5bce71f1ca85d9c15a246838fab4
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-07 14:06:29 +0700

    fix easy-ci

--- easy-ci.php
+++ easy-ci.php
@@ -3,8 +3,18 @@
 declare(strict_types=1);
 
 use Symplify\EasyCI\Config\EasyCIConfig;
+use Symplify\RuleDocGenerator\Contract\Category\CategoryInfererInterface;
+use Symplify\RuleDocGenerator\Contract\ConfigurableRuleInterface;
+use Symplify\RuleDocGenerator\Contract\RuleCodeSamplePrinterInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\ComposerJsonAwareCodeSample;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\ExtraFileCodeSample;
 
 return static function (EasyCIConfig $easyCIConfig): void {
     $easyCIConfig->typesToSkip([
+        CategoryInfererInterface::class,
+        ConfigurableRuleInterface::class,
+        RuleCodeSamplePrinterInterface::class,
+        ComposerJsonAwareCodeSample::class,
+        ExtraFileCodeSample::class,
     ]);
 };
