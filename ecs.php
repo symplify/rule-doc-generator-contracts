b33b804d53236cefc19c00009e2def3a6f0c26be
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-07 14:02:16 +0700

    add code analyiis workflow

---
+++ ecs.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/rector.php',
+        __DIR__ . '/easy-ci.php',
+        __DIR__ . '/src',
+    ]);
+
+    $ecsConfig->sets([
+        SetList::COMMON,
+        SetList::PSR_12,
+    ]);
+};
